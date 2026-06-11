b3524a8040321d586fc1a1b1e2233e1e3553ab02
MOKRANE Amel <amelmokrane05@gmail.com>
2026-06-11 09:16:35 +0100

    Fix number range and update user prompts in guessing game

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import random
 
-a = random.randint(0, 100)  # inclusive of both 0 and 100
+a = random.randint(0, 20)  # inclusive of both 0 and 100
+
+print("Welcome to this guessing number game")
+print("You have 5 chances to find the right number. Good luck!")
 
 try:
-    b = int(input("Give a number between 0 and 100: "))
-    if a == b:
-        print("You won!")
-    else:
+    i = 0
+    while i < 3:
+        b = int(input("Give a number between 0 and 20: "))
+
+        if a == b:
+            print("You won!")
+            break
+        elif a < b:
+            print("Too high!")
+        else:
+            print("Too low!")
+
+        i += 1
+
+        if i < 3:
+            print("Try again!")
+
+    if i == 3:
         print("You lost. The number was", a)
+
 except ValueError:
-    print("You need to enter a valid whole number")
+    print("You need to give a number")
